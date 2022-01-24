3a89d1a1a7c3b0e2cca866241e8f724dbf8fb87d
Josh <josherb4@gmail.com>
2022-01-24 15:13:43 -0500

    add basic image processing/response

--- server.js
+++ server.js
@@ -3,19 +3,28 @@
 const express = require('express')
 const processTitle = require('./lib/processTitle');
 const processQS = require('./lib/processQS');
+const generateImage = require('./lib/generateImage');
 
-const server = express()
-const port = 3030
+const server = express();
+const port = 3030;
 
 server.get('/', (req, res) => {
     res.send({ status: 'OK' });
 });
 
-server.get('/:title.jpg', processTitle, processQS, /* generateImage */ (req, res) => {
-    const postTitle = res.locals.title;
-    const postDate = res.locals.date;
-    const postSource = res.locals.source;
-    res.send({ postTitle, postDate, postSource });
+server.use((req, res, next) => {
+    console.log(`req: ${decodeURI(req.originalUrl)}`);
+    return next();
+});
+
+server.get('/:title.jpg', processTitle, processQS, generateImage, (req, res) => {
+    // finalize and send image
+    if (res.locals.finalImage) {
+        res.type('jpg');
+        res.send(res.locals.finalImage);
+    } else {
+        res.status(422).send('Unable to generate image');
+    }
 });
 
 server.listen(port, () => {
